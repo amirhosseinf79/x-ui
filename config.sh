1cd59403067fdd4e830e53e2d2094bbf26de02e4
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2022-12-23 23:09:59 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,8 @@
+apt-get update -y && apt-get upgrade -y
 apt install curl socat -y
+apt install curl -y
+
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 curl https://get.acme.sh | sh
 
 read -p "Enter Domain: " domain
@@ -11,5 +15,6 @@ echo Domain: $domain
 ~/.acme.sh/acme.sh --register-account -m $email
 ~/.acme.sh/acme.sh --issue -d $domain --standalone
 ~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
+read -p "Press enter to continue..."
 echo Setting BBR...
 wget -N --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && bash bbr.sh
