cfd687f0ba1d7ba9899924c91aa2ec18b33eeecc
Amirhossein <amirhosseinfarrokhmehr1379@gmail.com>
2022-12-24 11:57:55 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,16 +1,29 @@
 apt-get update -y && apt-get upgrade -y
+apt install resolvconf
 apt install curl socat -y
 apt install curl -y
 
-bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
-curl https://get.acme.sh | sh
+systemctl start resolvconf.service
+systemctl enable resolvconf.service
+systemctl status resolvconf.service
 
 read -p "Enter Domain: " domain
 read -p "Enter Email [Empty for default]: " email
 email=${email:-amirhosseinfr79@gmail.com}
+
 echo Email: $email
 echo Domain: $domain
+read -p "Press enter to continue..."
 
+echo Setting up DNS...
+nano /etc/resolvconf/resolv.conf.d/head
+systemctl restart resolvconf.service
+
+echo Setting up Panel...
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+curl https://get.acme.sh | sh
+
+echo Setting up SSL...
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 ~/.acme.sh/acme.sh --register-account -m $email
 ~/.acme.sh/acme.sh --issue -d $domain --standalone
